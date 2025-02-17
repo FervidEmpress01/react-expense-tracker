459c6338681b3331fa39e610396e9ba4937f7fa1
FervidEmpress01 <mauricio.carrion@epn.edu.ec>
2025-02-17 17:33:24 -0500

    Ultimos cambios

--- src/components/transactions/TransactionItem.jsx
+++ src/components/transactions/TransactionItem.jsx
@@ -1,21 +1,23 @@
 import { useGlobalState } from "../../context/GlobalState";
 
-export function TransctionItem({ transaction }) {
+export function TransactionItem({ transaction }) {
     const { deleteTransaction } = useGlobalState();
 
     return (
         <li className="bg-zinc-600 text-white px-3 py-1 rounded-lg mb-2 w-full 
         flex justify-between items-center">
             <p className="text-sm">{transaction.description}</p>
-            <div>
-                <span>${transaction.amount}</span>
-                <button onClick={() => {
-                    deleteTransaction(transaction.id)
-                }}>
+            <div className="flex items-center">
+                <span className="mr-4">${transaction.amount}</span>
+                <button 
+                    onClick={() => deleteTransaction(transaction.id)}
+                    className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded"
+                >
                     x
-                </button></div>
+                </button>
+            </div>
         </li>
     )
 }
 
-export default TransctionItem
+export default TransactionItem;
